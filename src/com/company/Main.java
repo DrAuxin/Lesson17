6f3b8eb0a7cb6117d2ead27f04b48a9b96efa072
Dr. Auxin <paul.torrillo@gmail.com>
2016-10-18 10:24:21 -0400

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	/* public boolean contains(String ss) takes a string and returns a boolean. This method can be accessed from anywhere. It is applied to a string object*/
+	/* public boolean contains(String ss) takes a string and returns a boolean. This method can be accessed from anywhere. It is applied to a string object.
+	* This method sees if a string contains another string within.*/
         String a = "welcome";
 boolean c = a.contains("wel");
         System.out.println(c);
