b93ff9d87a5934c80d2b7fabf9503009f21f092d
Dr. Auxin <paul.torrillo@gmail.com>
2016-10-18 10:19:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* public boolean contains(String ss) takes a string and returns a boolean. This method can be accessed from anywhere. It is applied to a string object*/
+        String a = "welcome";
+boolean c = a.contains("wel");
+        System.out.println(c);
+    }
+}
